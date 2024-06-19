043bd791a0938925cb6eca46b44fc01074ac502b
Charles Bergeron <chuck.bergeron@gmail.com>
2024-06-19 14:37:46 -0700

    finish writing the JSON objects to a file

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import chalk from "chalk";
+import { openSync, closeSync, appendFileSync } from "node:fs";
 
 const num = Number(process.argv[2]);
+
+const DESCRIPTION = `THIS IS THE D3SCRIPTION!`;
 
 export async function main() {
   console.log("");
@@ -18,6 +21,25 @@ export async function main() {
   console.log("");
   console.log(chalk.blue(`Generating JSON for ${num} NFTs`));
   console.log("");
+
+  type NFT = {
+    name: string;
+    description: string;
+    image: string;
+  };
+
+  let data: NFT[] = [];
+  for (let i = 0; i < num; i++) {
+    data.push({
+      name: `Play Test NFT #${i + 1}`,
+      description: DESCRIPTION,
+      image: "https://arweave.net/lo2NVO4upUtInwmGYKRdsJZ2rRurqwWnmpUi5USvQFs"
+    });
+  }
+
+  const filePointer = openSync(`batch.json`, "a");
+  appendFileSync(filePointer, JSON.stringify(data), "utf8");
+  closeSync(filePointer);
 }
 
 main();
