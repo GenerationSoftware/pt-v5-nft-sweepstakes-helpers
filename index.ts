a63b2a11054c527e48eab3d7c79b4116633550c3
Charles Bergeron <chuck.bergeron@gmail.com>
2024-07-18 13:23:29 -0700

    change from append to write file to overwrite existing files, accept a new outputFilename cmd line arg

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import chalk from "chalk";
 import { program } from "commander";
-import { openSync, closeSync, appendFileSync } from "node:fs";
+import { openSync, closeSync, writeFileSync } from "node:fs";
 
 program
   .requiredOption("-t, --total <number>")
   .requiredOption("-n, --name <name string>")
   .requiredOption("-d, --description <description string>")
-  .requiredOption("-i, --imageUrl <arweave / ipfs / http image url>");
+  .requiredOption("-i, --imageUrl <arweave / ipfs / http image url>")
+  .requiredOption("-o, --outputFilename <filename string>");
 
 program.parse();
 
@@ -15,6 +16,7 @@ const total = Number(options.total);
 const name = options.name;
 const description = options.description;
 const imageUrl = options.imageUrl;
+const filename = options.outputFilename;
 
 export async function main() {
   console.log("");
@@ -37,6 +39,7 @@ export async function main() {
   console.log(chalk.blue(`Name: ${name}`));
   console.log(chalk.blue(`Description: ${description}`));
   console.log(chalk.blue(`Image URL: ${imageUrl}`));
+  console.log(chalk.blue(`Output filename: ${filename}`));
   console.log("");
 
   type NFT = {
@@ -54,8 +57,8 @@ export async function main() {
     });
   }
 
-  const filePointer = openSync(`batch.json`, "a");
-  appendFileSync(filePointer, JSON.stringify(data), "utf8");
+  const filePointer = openSync(filename, "w");
+  writeFileSync(filePointer, JSON.stringify(data), "utf8");
   closeSync(filePointer);
 }
 
